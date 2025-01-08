87169d542786d926fbce0a0133e06e71c663b26c
lisi <lisi@qq.com>
2025-01-08 15:51:35 +0800

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import BooksList from './BooksList'
+// import BooksList from './BooksList'
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div>
       <h1>Books management</h1>
-      <BooksList />
+      <MyComponent />
     </div>
   );
 }
--- src/BooksList.jsx
+++ src/BooksList.jsx
@@ -1,38 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { produce } from 'immer';
 import './BooksList.css'
 import BooksListItem from './BooksListItem';
 const initialBooks = [
-  {
-    id: 1,
-    title: 'JavaScript—The Comprehensive Guide',
-    author: 'Philip Ackermann',
-    isbn: '978-3836286299',
-    rating: 5,
-  }, {
-    id: 2,
-    title: 'Clean Code',
-    author: 'Robert Martin',
-    isbn: '978-0132350884',
-    rating: 2
-  },
-  {
-    id: 3,
-    title: 'Design Patterns',
-    author: 'Erich Gamma',
-    isbn: '978-0201633610',
-    rating: 5,
-  },
+
 ];
 function BooksList() {
   const [books, setBooks] = useState(initialBooks);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBooks([
+        {
+          id: 1,
+          title: 'JavaScript - The Comprehensive Guide',
+          author: 'Philip Ackermann',
+          isbn: '978-3836286299',
+          rating: 5,
+        },
+      ]);
+    }, 2000);
+  }, []);
+
+  useEffect(() => {
+    console.log('Elements in the state: ', books.length);
+    console.log(
+      'Table rows: ',
+      document.querySelectorAll('tbody tr').length
+    );
+  });
 
   function handleRate(id, rating) {
     setBooks((prevState) => {
       return produce(prevState, (draftState) => {
         const index = draftState.findIndex((book) => book.id === id);
         draftState[index].rating = rating;
-        });
+      });
     });
   }
 
@@ -51,7 +54,7 @@ function BooksList() {
         </thead>
         <tbody>
           {books.map((book) => (
-            <BooksListItem key={book.id} book={book} onRate={handleRate}/>
+            <BooksListItem key={book.id} book={book} onRate={handleRate} />
           ))}
         </tbody></table>
     );
